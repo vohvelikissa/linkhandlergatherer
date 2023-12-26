c7391b7d4030c14841d74a208d917184838c3060
Pessi <--add>
2023-12-26 20:00:50 +0200

    added shit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int args, char * argv[]) {
+	if (args >= 2) {
+		char toBeExecuted[1024] = "w3m \"duckduckgo.com/?q=\"";
+		strcat(toBeExecuted, argv[1]);
+		strcat(toBeExecuted, " | grep \"www.\" > changethis.txt");
+		system(toBeExecuted);
+		system("sed -i 's/\\swww.//g' changethis.txt");
+		system("cat changethis.txt | awk '{$1=$1;print}'");
+		system("w3m $(cat changethis.txt)");
+	} else {
+		printf("INSERT A SEARCH TERM");
+	}
+}
